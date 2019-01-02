c327e64f2442855e2e9a0d20ce667813fc072c29
qincong <qin_cong@suixingpay.com>
2019-01-02 10:52:40 +0800

    first commit

---
+++ src/main/java/com/qincong/feignservice/FeignServiceApplication.java
@@ -0,0 +1,20 @@
+package com.qincong.feignservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@EnableEurekaServer
+@SpringBootApplication
+public class FeignServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignServiceApplication.class, args);
+	}
+
+}
+
